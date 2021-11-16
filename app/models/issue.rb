feb5eba891c0419880b297eff5bc01b77b6255ba
Florian <floho2016@yahoo.com>
2021-11-16 00:50:33 -0600

    created dropdown options for an issue's severity level and resolve status

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,6 @@
 class Issue < ApplicationRecord
-   
+  validates :title, presence: true
+
+  enum severity_level: {low: 'Low', medium: 'Medium', urgent: 'Urgent'}
+  enum resolve_status: {unresolved: 'Unresolved', resolved: 'Resolved'}
 end
